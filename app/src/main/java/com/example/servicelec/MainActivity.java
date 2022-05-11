58a0891faf319ffd190bb560baed58d261d61435
Moudyash <you@example.com>
2022-05-11 13:16:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/servicelec/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.servicelec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+Button btn1,btn2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1=findViewById(R.id.button);
+        btn2=findViewById(R.id.button2);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,MyService.class);
+                startService(i);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,MyService.class);
+                stopService(i);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/servicelec/MyService.java
@@ -0,0 +1,44 @@
+package com.example.servicelec;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    MediaPlayer mp ;
+    public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        mp = MediaPlayer.create(this, R.raw.b);
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                stopSelf();
+            }
+        });
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        mp.pause();
+
+        super.onDestroy();
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
